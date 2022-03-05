193fd6631224952a9a0bff04f1ff6c431a4cf983
VikasBenki <vikasmsbenki@gmail.com>
2022-03-05 23:28:15 +0530

    [VikasBenki] Adding the Validation password four

--- UserRegistrationRegularExPr/UserRegistrationRegularExPr/ValidationRegex.cs
+++ UserRegistrationRegularExPr/UserRegistrationRegularExPr/ValidationRegex.cs
@@ -43,10 +43,11 @@ namespace UserRegistrationRegularExPr
         }
         public void Checkpassward(string Passward)
         {
-            //string passwardpattern = "^[A-Za-z0-9]{8,20}$"; 
-            // string passwardpattern = "^(?=.*?[a-z])(?=.*?[A-Z])(?=.*?[0-9]).{8,}"; 
-            //string passwardpattern = "^(?=.*[A-Z])(?=.*[0-9]).{8,}$";
-            string passwardpattern = "^(?=.*[A-Z])(?=.*[a-z]{8,}).*$";
+            // string passwardpattern = "^[A-Za-z0-9]{8,20}$"; 
+            // string passwardpattern = "^(?=.*[A-Z])(?=.*[a-z]{8,}).*$";
+            //  string passwardpattern = "^(?=.*?[a-z])(?=.*?[A-Z])(?=.*?[0-9]).{8,}"; 
+            // string passwardpattern = "^(?=.*[A-Z])(?=.*[0-9]).{8,}$";
+            string passwardpattern = "^(?=.*[A-Z])(?=.*[0-9])(?=.*[&%$#@^*!~]).{8,}$";
             if (Regex.IsMatch(Passward, passwardpattern))
                 Console.WriteLine("Passward is valid");
             else
