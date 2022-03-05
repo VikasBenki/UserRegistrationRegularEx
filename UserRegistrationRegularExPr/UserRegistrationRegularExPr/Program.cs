394605da06a268ce08f48ec05a2f886a0d37e530
VikasBenki <vikasmsbenki@gmail.com>
2022-03-05 19:41:49 +0530

    [VikasBenki] Adding the UC1Validate first name

--- UserRegistrationRegularExPr/UserRegistrationRegularExPr/Program.cs
+++ UserRegistrationRegularExPr/UserRegistrationRegularExPr/Program.cs
@@ -11,7 +11,30 @@ namespace UserRegistrationRegularExPr
         static void Main(string[] args)
         {
             Console.WriteLine("Hello all welcome to User Registration Regex");
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("Enter Program number to get executed \n1. Validate first name \n2. Exit");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        Console.WriteLine("*First letter should be capital.");
+                        Console.WriteLine("*length should be Minimum three characters.");
+                        Console.WriteLine("Enter First name :");
+                        string fname = Console.ReadLine();
+                        ValidationRegex Checkpattern = new ValidationRegex();
+                        Checkpattern.Checkfirstname(fname);
+                        break;
+                    case 2:
+                        flag = false;
+                        break;
+                    default:
+                        Console.WriteLine("invalid option");
+                        break;
+                }
+            }
             Console.ReadLine();
         }
-    }
+    }          
 }
