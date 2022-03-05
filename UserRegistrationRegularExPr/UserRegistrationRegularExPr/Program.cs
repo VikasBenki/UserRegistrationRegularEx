828be6579c3098785938fb68b56d0b5134e1f984
VikasBenki <vikasmsbenki@gmail.com>
2022-03-05 19:28:31 +0530

    [VikasBenki] welcome message for the user registrarion prob

---
+++ UserRegistrationRegularExPr/UserRegistrationRegularExPr/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserRegistrationRegularExPr
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello all welcome to User Registration Regex");
+            Console.ReadLine();
+        }
+    }
+}
